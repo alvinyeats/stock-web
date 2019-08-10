9c233cf33b6c16525bc069e06db2a05ef564f6b6
alvin <alvinyeats@gmail.com>
2019-08-11 00:17:57 +0800

    add sidebar

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,10 @@ Vue.use(Router)
 /* Layout */
 import Layout from '@/layout'
 
+/* Router Modules */
+import componentsRouter from './modules/components'
+import chartsRouter from './modules/charts'
+
 /**
  * Note: sub-menu only appear when route children.length >= 1
  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
@@ -58,13 +62,202 @@ export const constantRoutes = [
   {
     path: '/market',
     component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: '市场风险指数', icon: 'dashboard' }
-    }]
+    name: 'Market',
+    meta: {
+      title: '市场风险指数',
+      icon: 'dashboard'
+    },
+    children: [
+      {
+        path: 'price',
+        component: () => import('@/views/dashboard/index'),
+        name: 'MarketPrice',
+        meta: {
+          title: '价格风险指数',
+          icon: 'dashboard'
+        }
+      },
+      {
+        path: 'deal',
+        component: () => import('@/views/dashboard/index'),
+        name: 'MarketDeal',
+        meta: {
+          title: '交易量风险指数',
+          icon: 'dashboard'
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/industry',
+    component: Layout,
+    name: 'Industry',
+    meta: {
+      title: '行业风险指数',
+      icon: 'dashboard'
+    },
+    children: [
+      {
+        path: 'price',
+        component: () => import('@/views/dashboard/index'),
+        name: 'IndustryPrice',
+        meta: {
+          title: '价格风险指数',
+          icon: 'dashboard'
+        }
+      },
+      {
+        path: 'deal',
+        component: () => import('@/views/dashboard/index'),
+        name: 'IndustryDeal',
+        meta: {
+          title: '交易量风险指数',
+          icon: 'dashboard'
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/stock',
+    component: Layout,
+    name: 'Stock',
+    meta: {
+      title: '个股风险指数',
+      icon: 'dashboard'
+    },
+    children: [
+      {
+        path: 'group',
+        component: () => import('@/views/dashboard/index'),
+        name: 'StockGroup',
+        meta: {
+          title: '模拟组合个股价格、交易量风险指数',
+          icon: 'dashboard'
+        }
+      },
+      {
+        path: 'single',
+        component: () => import('@/views/dashboard/index'),
+        name: 'StockSingle',
+        meta: {
+          title: '单只个股价格、交易量风险指数',
+          icon: 'dashboard'
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/recommand',
+    component: Layout,
+    name: 'Recommand',
+    meta: {
+      title: '个股推荐',
+      icon: 'dashboard'
+    },
+    children: [
+      {
+        path: 'pe',
+        component: () => import('@/views/dashboard/index'),
+        name: 'RecommandPriceEarning',
+        meta: {
+          title: '每日市盈率新低个股',
+          icon: 'dashboard'
+        }
+      },
+      {
+        path: 'history',
+        component: () => import('@/views/dashboard/index'),
+        name: 'RecommandHistory',
+        meta: {
+          title: '每日历史新高个股',
+          icon: 'dashboard'
+        }
+      },
+      {
+        path: 'macd',
+        component: () => import('@/views/dashboard/index'),
+        name: 'RecommandMACD',
+        meta: {
+          title: '每日MACD金叉个股',
+          icon: 'dashboard'
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/deal',
+    component: Layout,
+    name: 'Deal',
+    meta: {
+      title: '交易',
+      icon: 'dashboard'
+    },
+    children: [
+      {
+        path: 'stock',
+        component: () => import('@/views/dashboard/index'),
+        name: 'DealStock',
+        meta: {
+          title: '个股买进、卖出',
+          icon: 'dashboard'
+        }
+      },
+      {
+        path: 'history',
+        component: () => import('@/views/dashboard/index'),
+        name: 'DealHistory',
+        meta: {
+          title: '历史成交、收益率',
+          icon: 'dashboard'
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/permission',
+    component: Layout,
+    redirect: '/permission/page',
+    alwaysShow: true, // will always show the root menu
+    name: 'Permission',
+    meta: {
+      title: 'Permission',
+      icon: 'lock',
+      roles: ['admin', 'editor'] // you can set roles in root nav
+    },
+    children: [
+      {
+        path: 'page',
+        component: () => import('@/views/permission/page'),
+        name: 'PagePermission',
+        meta: {
+          title: 'Page Permission',
+          roles: ['admin'] // or you can only set roles in sub nav
+        }
+      },
+      {
+        path: 'directive',
+        component: () => import('@/views/permission/directive'),
+        name: 'DirectivePermission',
+        meta: {
+          title: 'Directive Permission'
+          // if do not set roles, means: this page does not require permission
+        }
+      },
+      {
+        path: 'role',
+        component: () => import('@/views/permission/role'),
+        name: 'RolePermission',
+        meta: {
+          title: 'Role Permission',
+          roles: ['admin']
+        }
+      }
+    ]
   },
 
   // 404 page must be placed at the end !!!
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,61 +1,31 @@
 <template>
   <div class="dashboard-container">
-    <!-- <div class="dashboard-text">name: {{ name }}</div> -->
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData" />
-    </el-row>
+    <component :is="currentRole" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import LineChart from './components/LineChart'
-
-const lineChartData = {
-  newVisitis: {
-    expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
-  },
-  messages: {
-    expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
-  },
-  purchases: {
-    expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
-  },
-  shoppings: {
-    expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
+import adminDashboard from './admin'
+import editorDashboard from './editor'
 
 export default {
   name: 'Dashboard',
-  components: {
-    LineChart
-  },
+  components: { adminDashboard, editorDashboard },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
+      currentRole: 'adminDashboard'
     }
   },
   computed: {
     ...mapGetters([
-      'name'
+      'roles'
     ])
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
-</style>
